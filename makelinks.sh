2c87c87c7b32fd72c57588a056135beda5870e68
Lasse Magnussen <lasse.magnussen@finn.no>
2013-04-25 21:19:35 +0200

    Stole some stuff from github dotfiles.

--- makelinks.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-DIR="$( cd "$( dirname "$0" )" && pwd )"
-ln -fs $HOME/.dotfiles/.bash_profile ~/.bash_profile
-ln -fs $HOME/.dotfiles/.bashrc ~/.bashrc
-ln -fs $HOME/.dotfiles/.vim ~/.vim
-ln -fs $HOME/.dotfiles/.gitconfig ~/.gitconfig
