c403a3f312e21cc47f35d21d1be3eed88082332c
Lasse Bjørknes Magnussen <lasse.magnussen@vipps.no>
2021-02-07 20:49:32 +0100

    Various amendments.

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -8,12 +8,11 @@
 # Check for Homebrew
 if test ! $(which brew)
 then
-	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 	brew doctor
 fi
 
 # Install homebrew packages
-brew tap Homebrew/bundle
 brew bundle --file=homebrew/brewfile
 
 exit 0
